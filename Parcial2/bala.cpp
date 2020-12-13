4ff7a6f7dfc21882633c8fd8bf156b9bc7f37bda
angeelorenaoc <angee.ocampo@udea.edu.co>
2020-12-13 09:12:27 -0500

    Desarrollo de los metodos de la clase bala, punto 2 y 3 del parcial

--- Parcial2/bala.cpp
+++ Parcial2/bala.cpp
@@ -1,4 +1,8 @@
 #include "bala.h"
+Bala::Bala()
+{
+
+}
 
 float Bala::getV0() const
 {
@@ -53,7 +57,88 @@ void Bala::DisparoOfensivo(Canion DO, Canion DD, int V0_)
     }
 }
 
-Bala::Bala()
+void Bala::DisparoDefensivo(Canion DO, Canion DD, int V0_)
 {
+    int flag = 0;
+    float x,y;
+    float Vxo,Vy0;
+    int V0o = 0;
+    int t = 0;
+    int angle = 0;
+    for(V0o = V0_; ; V0o += 5){
+        for(angle = 0; angle < 90; angle++){
+            Vxo = V0o*cos((angle+90)*pi/180);
+            Vy0 = V0o*sin((angle+90)*pi/180);
+            x = 0.0;
+            y = 0.0;
+            for(t = 0; ; t++){
+                x = Vxo*t;
+                y = DD.getY0() + Vy0*t -(0.5*G*t*t);
+                if(sqrt(pow((DO.getX0() - x),2)+pow((DO.getY0() - y),2)) < DD.getD0()){
+                    if(y<0) y = 0;
+                    flag += 1;
+                    cout << "Disparo " << flag << endl;
+                    ImprimirResultados(angle, V0o, x, y, t);
+                    V0o += 50;
+                    break;
+                }
+                if(y < 0){
+                    break;
+                }
+            }
+            if(flag == 3) break;
 
+        }
+        if(flag == 3) break;
+    }
+    if(flag != 3){
+        cout << "No impacto en los disparos esperados"<< endl;
+    }
 }
+
+void Bala::DisparoDefensivo2(Canion DO, Canion DD, int V00, int anguloo, int vo0)
+{
+    int flag = 0;
+    float x,y,x2,y2;
+    float Vxo,Vy0, Vxoo,Vyoo;
+    int V0o = 0;
+    float t = 0;
+    int angle = 0;
+    Vxoo = vo0*cos((anguloo)*pi/180);
+    Vyoo = vo0*sin((anguloo)*pi/180);
+    for(V0o = V00; ; V0o += 5){
+        for(angle = 0; angle < 90; angle++){
+            Vxo = V0o*cos((angle+90)*pi/180);
+            Vy0 = V0o*sin((angle+90)*pi/180);
+            x = 0.0;
+            y = 0.0;
+            x2 = 0.0;
+            y2 = 0.0;
+            for(t = 0; ; t++){
+                x2 = Vxoo*(t+2.5);
+                y2 = DO.getY0() + Vyoo*(t+2.5) -(0.5*G*(t+2.5)*(t+2.5));
+                x = DD.getX0()+Vxo*t;
+                y = DD.getY0() + Vy0*t -(0.5*G*t*t);
+                if(sqrt(pow((x2 - x),2)+pow((y2 - y),2)) < DD.getD0()){
+                    if(y<0) y = 0;
+                    ImprimirResultados(anguloo,vo0,x2,y2,t+2.5);
+                    cout << "_________________________________"<<endl;
+                    ImprimirResultados(angle, V0o, x, y, t);
+                    flag += 1;
+                    V0o += 50;
+                    break;
+                }
+                if(y < 0){
+                    break;
+                }
+            }
+            if(flag == 3) break;
+
+        }
+        if(flag == 3) break;
+    }
+    if(flag != 3){
+        cout << "No impacto en los disparos esperados"<< endl;
+    }
+}
+
